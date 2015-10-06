aa0a6d006c2ec2e1762e555559bdded341a94acf
Vlad Vergu <v.vergu@gmail.com>
2015-10-06 14:36:28 +0300

    Example of how to run a benchmark for data collection

--- org.metaborg.lang.sl.interp/src/org/metaborg/lang/sl/interp/Main.java
+++ org.metaborg.lang.sl.interp/src/org/metaborg/lang/sl/interp/Main.java
@@ -10,7 +10,17 @@ public class Main {
 		String file = args[0];
 		SLLanguage language = SLLanguage.INSTANCE;
 		Callable<R_init_U> callee = language.getCallable(file);
+
 		callee.call();
+		// BenchmarkConfiguration benchconfig = new BenchmarkConfiguration();
+		// benchconfig.warmuprounds = 100;
+		// benchconfig.repetitions = 100;
+		// benchconfig.dataFile = new File("data.csv");
+		//
+		// Benchmark benchmark = new Benchmark(callee, benchconfig);
+		// benchmark.execute();
+		//
+		// benchmark.writeToFile();
 	}
 
 }
