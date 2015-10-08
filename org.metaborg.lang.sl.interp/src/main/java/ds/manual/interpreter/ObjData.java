e6dd67c19d752095b1a450212f9f3e1751c93aed
Vlad Vergu <v.vergu@gmail.com>
2015-10-08 12:34:04 +0300

    Implement ObjData

--- org.metaborg.lang.sl.interp/src/main/java/ds/manual/interpreter/ObjData.java
+++ org.metaborg.lang.sl.interp/src/main/java/ds/manual/interpreter/ObjData.java
@@ -1,19 +1,27 @@
 package ds.manual.interpreter;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.oracle.truffle.api.source.SourceSection;
+
 import ds.generated.interpreter.A_V;
+import ds.generated.interpreter.NullV_0;
 
 public class ObjData {
 
-	// FIXME implement
+	private Map<String, A_V> store = new HashMap<>();
 
 	public A_V writeProp(String prop, A_V v) {
-		// TODO Auto-generated method stub
-		return null;
+		return store.put(prop, v);
 	}
 
 	public A_V readProp(String prop) {
-		// TODO Auto-generated method stub
-		return null;
+		A_V v = store.get(prop);
+		if (v == null) {
+			v = new NullV_0(SourceSection.createUnavailable("cucu", "baubau"));
+		}
+		return v;
 	}
 
 }
