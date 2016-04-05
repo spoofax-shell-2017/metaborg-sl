692211c177730f031472e77550b57f8e114bf47e
Vlad Vergu <v.vergu@gmail.com>
2016-04-05 23:07:23 +0200

    simple evaluation fix for cli runner

--- org.metaborg.lang.sl.interp/sl.sh
+++ org.metaborg.lang.sl.interp/sl.sh
@@ -5,16 +5,23 @@ set -e
 BASE_DIR=`pwd`
 
 GRAAL_FLAGS='
-
-  -G:+TraceTruffleCompilation
-  -G:+TraceTrufflePerformanceWarnings
-  -G:+FailedLoopExplosionIsFatal
-  -G:+TruffleCompilationExceptionsAreFatal
+  -ea
+  -G:TruffleCompilationThreshold=2
+  -G:TruffleMaximumRecursiveInlining=10
   '
-  
+
+# GRAAL_FLAGS='
+#   -G:TruffleCompilationThreshold=2
+#   -G:+TraceTruffleCompilation
+#   -G:+TraceTrufflePerformanceWarnings
+#   -G:+FailedLoopExplosionIsFatal
+#   -G:+TruffleCompilationExceptionsAreFatal
+#   '
+# TODO: disable background compilation
+
 # -ea
 # -G:TruffleMaximumRecursiveInlining=10
-# -G:TruffleCompilationThreshold=2
+
 #   -G:+TraceTruffleCompilation
 #   -G:+TraceTruffleCompilationCallTree
 #   -G:+TraceTruffleInlining
@@ -29,7 +36,6 @@ GRAAL_SUITE='/Users/vladvergu/tud/slde/software/truffle/graal-root/graal-core'
 CLASSPATH="$1"
 
 cp data.csv.model $3
-
   mx --primary-suite-path $GRAAL_SUITE \
     --vm server vm $GRAAL_FLAGS -Xss32m \
     -Xbootclasspath/a:target/classes:$CLASSPATH \
