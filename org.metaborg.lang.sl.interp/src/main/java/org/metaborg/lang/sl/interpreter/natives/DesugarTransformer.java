bc0fb400fa7094bcceedfbba13267e0981d3ac2d
Vlad Vergu <v.vergu@gmail.com>
2016-04-25 12:57:01 +0300

    Update for moved ITermTransformer

--- org.metaborg.lang.sl.interp/src/main/java/org/metaborg/lang/sl/interpreter/natives/DesugarTransformer.java
+++ org.metaborg.lang.sl.interp/src/main/java/org/metaborg/lang/sl/interpreter/natives/DesugarTransformer.java
@@ -2,7 +2,7 @@ package org.metaborg.lang.sl.interpreter.natives;
 
 import org.metaborg.lang.sl.desugar.desugar;
 import org.metaborg.lang.sl.desugar.desugar_all_0_0;
-import org.metaborg.meta.lang.dynsem.interpreter.ITermTransformer;
+import org.metaborg.meta.lang.dynsem.interpreter.terms.ITermTransformer;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 
 public class DesugarTransformer implements ITermTransformer {
